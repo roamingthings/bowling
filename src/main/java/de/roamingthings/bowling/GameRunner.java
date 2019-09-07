be7f14207afd0bcb44ca07f6f4fd52fb59b97ace
Alexander Sparkowsky <info@roamingthings.de>
2019-09-07 20:39:24 +0200

    Erfragt die Eingabe von Würfen

--- src/main/java/de/roamingthings/bowling/GameRunner.java
+++ src/main/java/de/roamingthings/bowling/GameRunner.java
@@ -1,7 +1,16 @@
 package de.roamingthings.bowling;
 
+import java.util.Scanner;
+
 public class GameRunner {
     public static void main(String[] args) {
+        System.out.println("Bitte geben Sie die Ergebnisse der Würfe als kommaseparierte Liste ein:");
 
+        String line = readLine();
+    }
+
+    private static String readLine() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 }
