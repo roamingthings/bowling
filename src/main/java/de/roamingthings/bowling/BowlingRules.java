c3578ac73222face4814d5ec7e8d05f1c1b4c81c
Alexander Sparkowsky <info@roamingthings.de>
2019-09-07 22:48:20 +0200

    Refactoring: Implementierung für alle Frames zusammengefasst

--- src/main/java/de/roamingthings/bowling/BowlingRules.java
+++ src/main/java/de/roamingthings/bowling/BowlingRules.java
@@ -8,33 +8,34 @@ public class BowlingRules {
         int currentMoveIndex = 0;
         int totalPoints = 0;
 
-        for (int currentFrame = 0; currentFrame < MAX_FRAME_NUMBER - 1; currentFrame++) {
+        for (int currentFrame = 1; currentFrame <= MAX_FRAME_NUMBER; currentFrame++) {
             if (game.isSpikeFrameAt(currentMoveIndex)) {
                 totalPoints += game.getSumOfSpikeFrameAt(currentMoveIndex);
-                currentMoveIndex += 1;
+                currentMoveIndex += numberOfMovesToAdvanceForSpike(currentFrame);
             } else if (game.isSpareFrameAt(currentMoveIndex)) {
                 totalPoints += game.getSumOfSpareFrameAt(currentMoveIndex);
-                currentMoveIndex += 2;
+                currentMoveIndex += numberOfMovesToAdvanceForSpare(currentFrame);
             } else {
                 totalPoints += game.getSumOfOpenFrameAt(currentMoveIndex);
                 currentMoveIndex += 2;
             }
         }
 
-        if (game.isSpikeFrameAt(currentMoveIndex)) {
-            totalPoints += game.getSumOfSpikeFrameAt(currentMoveIndex);
-            currentMoveIndex += 3;
-        } else if (game.isSpareFrameAt(currentMoveIndex)) {
-            totalPoints += game.getSumOfSpareFrameAt(currentMoveIndex);
-            currentMoveIndex += 3;
-        } else {
-            totalPoints += game.getSumOfOpenFrameAt(currentMoveIndex);
-            currentMoveIndex += 2;
-        }
-
         failIfMovesLeft(game, currentMoveIndex);
 
         return totalPoints;
+    }
+
+    private static int numberOfMovesToAdvanceForSpare(int frameNumber) {
+        return isLastFrame(frameNumber)? 3: 2;
+    }
+
+    private static int numberOfMovesToAdvanceForSpike(int frameNumber) {
+        return isLastFrame(frameNumber) ? 3 : 1;
+    }
+
+    private static boolean isLastFrame(int currentFrame) {
+        return currentFrame == MAX_FRAME_NUMBER;
     }
 
     private static void failIfMovesLeft(Game game, int currentMoveIndex) {
