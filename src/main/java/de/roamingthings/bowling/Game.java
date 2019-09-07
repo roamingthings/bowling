c03f2ce6b54b143e94de06770b025563a6ae9d3c
Alexander Sparkowsky <info@roamingthings.de>
2019-09-07 21:03:39 +0200

    Refactoring: GameFactory eingeführt

--- src/main/java/de/roamingthings/bowling/Game.java
+++ src/main/java/de/roamingthings/bowling/Game.java
@@ -4,28 +4,29 @@ import static java.util.Arrays.stream;
 
 public class Game {
 
-    private final int[] moves;
+    public static class GameFactory {
+        public static Game toGame(String input) {
+            int[] moves = convertToArray(input);
+            failOnInvalidValues(moves);
+            return new Game(moves);
+        }
 
-    public static Game fromInput(String input) {
-        int[] moves = convertToArray(input);
-        failOnInvalidValues(moves);
-        return new Game(moves);
-    }
+        private static int[] convertToArray(String input) {
+            return stream(input.split(","))
+                    .map(String::trim)
+                    .mapToInt(Integer::new)
+                    .toArray();
+        }
 
-    private static int[] convertToArray(String input) {
-        return stream(input.split(","))
-                .map(String::trim)
-                .mapToInt(Integer::new)
-                .toArray();
-    }
-
-    private static void failOnInvalidValues(int[] moves) {
-        boolean invalidMoves = stream(moves).anyMatch(pins -> pins < 0 || pins > 10);
-        if (invalidMoves) {
-            throw new InvalidPinsValueException();
+        private static void failOnInvalidValues(int[] moves) {
+            boolean invalidMoves = stream(moves).anyMatch(pins -> pins < 0 || pins > 10);
+            if (invalidMoves) {
+                throw new InvalidPinsValueException();
+            }
         }
     }
 
+    private final int[] moves;
 
     private Game(int[] moves) {
         this.moves = moves;
--- src/test/java/de/roamingthings/bowling/GameFactoryTest.java
+++ src/test/java/de/roamingthings/bowling/GameFactoryTest.java
@@ -2,14 +2,15 @@ package de.roamingthings.bowling;
 
 import org.junit.jupiter.api.Test;
 
+import static de.roamingthings.bowling.Game.GameFactory.toGame;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class GameTest {
+class GameFactoryTest {
 
     @Test
     void shouldConvertInput() {
-        Game game = Game.fromInput("1, 2, 3");
+        Game game = toGame("1, 2, 3");
 
         int pins = game.getPinsForMove(0);
 
@@ -18,13 +19,13 @@ class GameTest {
 
     @Test
     void shouldRejectInputWithValuesTooSmall() {
-        assertThatThrownBy(() -> Game.fromInput("1, 2, 3, -1"))
+        assertThatThrownBy(() -> toGame("1, 2, 3, -1"))
                 .isInstanceOf(InvalidPinsValueException.class);
     }
 
     @Test
     void shouldRejectInputWithValuesTooBig() {
-        assertThatThrownBy(() -> Game.fromInput("1, 2, 11, 3"))
+        assertThatThrownBy(() -> toGame("1, 2, 11, 3"))
                 .isInstanceOf(InvalidPinsValueException.class);
     }
 
